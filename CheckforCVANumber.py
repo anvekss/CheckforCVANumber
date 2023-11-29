e054a514be09410a9af5bb0b552bc8e2e8243454
anvekss <56787350+anvekss@users.noreply.github.com>
2023-11-29 20:10:37 +0530

    Update CheckforCVANumber.py

--- CheckforCVANumber.py
+++ CheckforCVANumber.py
@@ -47,7 +47,9 @@ AtifactoryBuild = getzipurllocation(ArtifactoryLocation)
 
 if (InstalledBuild == AtifactoryBuild ):
     print("Both the Build matches")
+    return(200)
 else:
     print("Builds are different")
+    return(400)
    
 
